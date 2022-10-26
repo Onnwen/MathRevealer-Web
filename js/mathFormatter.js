b3ec5eeeb5876caa4af508c2784ad7989a078beb
Onnwen <onnwen.cassitto@icloud.com>
2022-10-26 21:18:22 +0200

    Convertite funzioni alla classe MathFormatter, risolti bugs

--- js/mathFormatter.js
+++ js/mathFormatter.js
@@ -2,52 +2,69 @@ const operations = ["+", "-", "*", "/", "^"];
 const openingBrackets = ["("];
 const closingBrackets = [")"];
 
-function getTree(expression) {
-    let tree = [""]
-    let actualDepthIndex = 0;
-    let lastCharIsBracket = false;
+class MathExpression {
+    constructor(expression) {
+        this.expression = new MathFormatter(expression).getResult();
+    }
 
-    for (let charIndex = 0; charIndex < expression.length; charIndex++) {
-        let isOperationSymbol = operations.indexOf(expression[charIndex]) !== -1;
-        let isOpeningBracket = openingBrackets.indexOf(expression[charIndex]) !== -1;
-        let isClosingBracket = closingBrackets.indexOf(expression[charIndex]) !== -1;
+    getHtml() {
+        return new HtmlFormatter(this.expression).getResult();
+    }
+}
 
-        if (isOperationSymbol) {
-            append(tree, expression[charIndex], lastCharIsBracket)
-            lastCharIsBracket = false;
-        } else if (isOpeningBracket) {
-            if (operations.indexOf(expression[charIndex - 1]) === -1) {
-                append(tree, "*");
+class MathFormatter {
+    constructor(expression) {
+        let tree = [""]
+        let lastCharIsBracket = false;
+
+        for (let charIndex = 0; charIndex < expression.length; charIndex++) {
+            let isOperationSymbol = operations.indexOf(expression[charIndex]) !== -1;
+            let isOpeningBracket = openingBrackets.indexOf(expression[charIndex]) !== -1;
+            let isClosingBracket = closingBrackets.indexOf(expression[charIndex]) !== -1;
+
+            if (isOperationSymbol) {
+                this.append(tree, expression[charIndex], lastCharIsBracket)
+                lastCharIsBracket = false;
+            } else if (isOpeningBracket) {
+                if (operations.indexOf(expression[charIndex - 1]) === -1 && charIndex > 0 && openingBrackets.indexOf(expression[charIndex - 1]) === -1) {
+                    console.log(openingBrackets.indexOf(expression[charIndex - 1]))
+                    this.append(tree, "*");
+                }
+                this.append(tree, [expression[charIndex]], lastCharIsBracket)
+                this.append(tree, "");
+                lastCharIsBracket = false;
+            } else if (isClosingBracket) {
+                this.append(tree, expression[charIndex], lastCharIsBracket)
+                lastCharIsBracket = true;
+            } else {
+                if (operations.indexOf(expression[charIndex - 1]) !== -1) {
+                    this.append(tree, "");
+                }
+                this.appendInLast(tree, expression[charIndex])
+                lastCharIsBracket = false;
             }
-            append(tree, [expression[charIndex]], lastCharIsBracket)
-            lastCharIsBracket = false;
-        } else if (isClosingBracket) {
-            append(tree, expression[charIndex], lastCharIsBracket)
-            lastCharIsBracket = true;
-        } else {
-            if (operations.indexOf(expression[charIndex - 1]) !== -1) {
-                append(tree, "");
-            }
-            appendInLast(tree, expression[charIndex])
-            lastCharIsBracket = false;
         }
+
+        this.tree=tree;
     }
 
-    return tree;
-}
-
-function appendInLast(array, value) {
-    let actualArray = array;
-    while (Array.isArray(actualArray[actualArray.length - 1])) {
-        actualArray = actualArray[actualArray.length - 1];
+    getResult() {
+        return this.tree
     }
-    actualArray[actualArray.length - 1] += value;
-}
 
-function append(array, value, back = false) {
-    let actualArray = array;
-    while (back ? Array.isArray(actualArray[actualArray.length - 1][actualArray[actualArray.length - 1].length - 1]) : Array.isArray(actualArray[actualArray.length - 1])) {
-        actualArray = actualArray[actualArray.length - 1];
+    appendInLast(array, value) {
+        let actualArray = array;
+        while (Array.isArray(actualArray[actualArray.length - 1])) {
+            actualArray = actualArray[actualArray.length - 1];
+        }
+        actualArray[actualArray.length - 1] += value;
     }
-    actualArray.push(value);
+
+    append(array, value, back = false) {
+        let actualArray = array;
+        while (back ? Array.isArray(actualArray[actualArray.length - 1][actualArray[actualArray.length - 1].length - 1]) : Array.isArray(actualArray[actualArray.length - 1])) {
+            actualArray = actualArray[actualArray.length - 1];
+        }
+        actualArray.push(value);
+    }
 }
