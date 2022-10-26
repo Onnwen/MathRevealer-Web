ac6b699e0bfd48f790aa6276c03441a2a368325b
Onnwen <onnwen.cassitto@icloud.com>
2022-10-26 21:18:06 +0200

    Convertite funzioni alla classe HtmlFormatter

--- js/htmlFormatter.js
+++ js/htmlFormatter.js
@@ -1,37 +1,44 @@
-function getHtml(tree) {
-    let html = "";
-    if (Array.isArray(tree)) {
-        for (let branchIndex = 0; branchIndex < tree.length; branchIndex++) {
-            if (!Array.isArray(tree[branchIndex])) {
-                if (tree[branchIndex] === "^") {
-                    html += "<sup>" + getHtml(tree[branchIndex + 1]) + "</sup>";
+class HtmlFormatter {
+    constructor(tree) {
+        this.html = this.generateHtml(tree)
+    }
+
+    getResult() {
+        return this.html;
+    }
+
+    generateHtml(branches) {
+        let html = "";
+        if (Array.isArray(branches)) {
+            for (let branchIndex = 0; branchIndex < branches.length; branchIndex++) {
+                if (branches[branchIndex+1] === "/") {
+                    html += "<div class='frac'><span>" + this.generateHtml(branches[branchIndex]) + "</span><span class='symbol'>/</span><span class='bottom'>" + (branches[branchIndex+2] !== undefined ? this.generateHtml(branches[branchIndex+2]) : '...') + "</span></div>";
                     branchIndex++;
-                }
-                else if (tree[branchIndex+1] === "/") {
-                    html += "<div class='frac'><span>" + tree[branchIndex] + "</span><span class='symbol'>/</span><span class='bottom'>" + (tree[branchIndex+2] !== undefined ? tree[branchIndex+2] : '...') + "</span></div>";
                     branchIndex++;
-                    branchIndex++;
-                }
-                else if (tree[branchIndex] === "+") {
-                    html += "&plus;";
-                }
-                else if (tree[branchIndex] === "-") {
-                    html += "&minus;";
-                }
-                else if (tree[branchIndex] === "*") {
-                    html += "&times;";
                 }
                 else {
-                    html += tree[branchIndex];
+                    if (!Array.isArray(branches[branchIndex])) {
+                        if (branches[branchIndex] === "^") {
+                            html += "<sup>" + this.generateHtml(branches[branchIndex + 1]) + "</sup>";
+                            branchIndex++;
+                        } else if (branches[branchIndex] === "+") {
+                            html += "&plus;";
+                        } else if (branches[branchIndex] === "-") {
+                            html += "&minus;";
+                        } else if (branches[branchIndex] === "*") {
+                            html += "&times;";
+                        } else {
+                            html += branches[branchIndex];
+                        }
+                    } else {
+                        html += this.generateHtml(branches[branchIndex]);
+                    }
                 }
-            } else {
-                html += getHtml(tree[branchIndex][tree]);
             }
+            return html;
         }
-        console.log(html);
-        return html;
-    }
-    else {
-        return html + tree;
+        else {
+            return html + branches;
+        }
     }
 }
