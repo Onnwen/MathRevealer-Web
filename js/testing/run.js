9a1a0c4f55443d2d5da1f08b97dc74a35e1df11c
Onnwen <onnwen.cassitto@icloud.com>
2022-11-03 21:53:34 +0100

    Cambiamenti minori

--- js/testing/run.js
+++ js/testing/run.js
@@ -1,2 +1,2 @@
-const mathFormatter = require('../mathFormatter');
-console.log(new mathFormatter("1+2+3").getResult());
+const MathExpression = require('../MathExpression.js');
+console.log(new MathExpression("1-2+(3-4)+3").getJson());
