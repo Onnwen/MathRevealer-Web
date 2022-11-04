c31ed1cf3549f7c27c3ea5a63f8300ab1232290e
Onnwen <onnwen.cassitto@icloud.com>
2022-11-04 12:16:21 +0100

    Aggiornati test cases

---
+++ js/testing/MathExpression.test.js
@@ -0,0 +1,13 @@
+const MathExpression = require('../MathExpression.js');
+
+test('formatting: "1-2"', () => {
+    expect(new MathExpression("1-2").getJson()).toEqual('{"level":{"level":["1","-","2"],"brackets":"","error":""}}');
+});
+
+test('formatting: "1-2+(3-4)"', () => {
+    expect(new MathExpression("1-2+(3-4)").getJson()).toEqual('{"level":{"level":["1","-","2","+",{"level":["3","-","4"],"brackets":"()","error":""}],"brackets":"","error":""}}');
+});
+
+test('formatting: "1-2+(3-4)+3"', () => {
+    expect(new MathExpression("1-2+(3-4)+3").getJson()).toEqual('{"level":{"level":["1","-","2","+",{"level":["3","-","4"],"brackets":"()","error":""},"+","3"],"brackets":"","error":""}}');
+});
