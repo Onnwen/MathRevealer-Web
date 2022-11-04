bdf454ae86ff5963c4fb569fb817cb37881a256c
Onnwen <onnwen.cassitto@icloud.com>
2022-11-04 12:16:55 +0100

    Aggiornati test cases

--- js/testing/mathFormatter.test.js
+++
@@ -1,9 +0,0 @@
-const mathFormatter = require('../mathFormatter');
-
-test('formatting: "1+2"', () => {
-    expect(new mathFormatter("1+2")).toEqual({"tree": ["1+2"]});
-});
-
-test('formatting: "1+(2/3)"', () => {
-    expect(new mathFormatter("1+2")).toEqual({"tree": ["1", "+", ["2", "/", "3"]]});
-});
