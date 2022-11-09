aeb4118dab8a201ff3c3d25420ad273d3f05be11
Onnwen <onnwen.cassitto@icloud.com>
2022-11-09 15:45:05 +0100

    Gestione dei moduli effettuata con "import/export" e test case migrati da Jest a Jasmine
    This reverts commit c65083ae3bb987ffd33d4fc8a1db9b541f211d67.

--- spec/HtmlFormatter.spec.js
+++ spec/HtmlFormatter.spec.js
@@ -1,26 +1,25 @@
 // Testing HtmlFormatter trough MathExpression that invoke HtmlFormatter instance.
 
-import {describe, expect, test} from '@jest/globals';
-import {MathExpression} from '../MathExpression.js';
+import {MathExpression} from '../js/MathExpression.js';
 
 describe('HtmlFormatter', () => {
-    test('formatting: "1-2"', () => {
+    it('formatting: "1-2"', () => {
         expect(new MathExpression("1-2").getHtml()).toEqual('1&minus;2');
     });
 
-    test('formatting: "1+2"', () => {
+    it('formatting: "1+2"', () => {
         expect(new MathExpression("1+2").getHtml()).toEqual('1&plus;2');
     });
 
-    test('formatting: "1*2"', () => {
+    it('formatting: "1*2"', () => {
         expect(new MathExpression("1*2").getHtml()).toEqual('1&times;2');
     });
 
-    test('formatting: "1^2"', () => {
+    it('formatting: "1^2"', () => {
         expect(new MathExpression("1^2").getHtml()).toEqual('1<sup>2</sup>');
     });
 
-    test('formatting: "1/2"', () => {
+    it('formatting: "1/2"', () => {
         expect(new MathExpression("1/2").getHtml()).toEqual("<div class=\'frac\'><span>1</span><span class=\'symbol\'>/</span><span class=\'bottom\'>2</span></div>");
     });
 });
--- spec/MathExpression.spec.js
+++ spec/MathExpression.spec.js
@@ -1,28 +1,27 @@
-import {describe, expect, test} from '@jest/globals';
-import {MathExpression} from '../MathExpression.js';
+import {MathExpression} from '../js/MathExpression.js';
 
 describe('MathExpression', () => {
-    test('formatting: "1-2"', () => {
+    it('formatting: "1-2"', () => {
         expect(new MathExpression("1-2").getJson()).toEqual('{"level":{"level":["1","-","2"],"brackets":"","error":""}}');
     });
 
-    test('formatting: "1-2+(3-4)"', () => {
+    it('formatting: "1-2+(3-4)"', () => {
         expect(new MathExpression("1-2+(3-4)").getJson()).toEqual('{"level":{"level":["1","-","2","+",{"level":["3","-","4"],"brackets":"()","error":""}],"brackets":"","error":""}}');
     });
 
-    test('formatting: "1-2+(3-4)+3"', () => {
+    it('formatting: "1-2+(3-4)+3"', () => {
         expect(new MathExpression("1-2+(3-4)+3").getJson()).toEqual('{"level":{"level":["1","-","2","+",{"level":["3","-","4"],"brackets":"()","error":""},"+","3"],"brackets":"","error":""}}');
     });
 
-    test('formatting: "1-2(3-4)+3"', () => {
+    it('formatting: "1-2(3-4)+3"', () => {
         expect(new MathExpression("1-2(3-4)+3").getJson()).toEqual('{"level":{"level":["1","-","2","*",{"level":["3","-","4"],"brackets":"()","error":""},"+","3"],"brackets":"","error":""}}');
     });
 
-    test('formatting: "2*x"', () => {
+    it('formatting: "2*x"', () => {
         expect(new MathExpression("2*x").getJson()).toEqual('{"level":{"level":["2","*","x"],"brackets":"","error":""}}');
     });
 
-    test('formatting: "2x"', () => {
+    it('formatting: "2x"', () => {
         expect(new MathExpression("2x").getJson()).toEqual('{"level":{"level":["2","*","x"],"brackets":"","error":""}}');
     });
 });
