8505903767c65a297d4b9ac79580b95ce0ff36ba
Onnwen <onnwen.cassitto@icloud.com>
2022-11-10 20:56:20 +0100

    Porzioni di espressioni errare evidenziate con tooltip

--- src/HtmlFormatter.js
+++ src/HtmlFormatter.js
@@ -10,8 +10,15 @@ export class HtmlFormatter {
     generateHtml(mathLevel) {
         let html = "";
         if (typeof mathLevel === 'object' && mathLevel !== undefined) {
+            if (mathLevel.error !== "") {
+                html += '<a href="#" data-toggle="tooltip" title="' + mathLevel.error + '" class="errorText">'
+            }
+            html += mathLevel.getBrackets()[0] !== undefined ? mathLevel.getBrackets()[0] : "";
             for (let charIndex = 0; charIndex < mathLevel.getLevel().length; charIndex++) {
-                if (mathLevel.getLevel()[charIndex+1] === "/") {
+                if (typeof mathLevel.getLevel()[charIndex] === 'object' && mathLevel.getLevel()[charIndex] !== undefined) {
+                    html += this.generateHtml(mathLevel.getLevel()[charIndex]);
+                }
+                else if (mathLevel.getLevel()[charIndex+1] === "/") {
                     html += "<div class='frac'><span>" + this.generateHtml(mathLevel.getLevel()[charIndex]) + "</span><span class='symbol'>/</span><span class='bottom'>" + (mathLevel.getLevel()[charIndex+2] !== undefined ? this.generateHtml(mathLevel.getLevel()[charIndex+2]) : '...') + "</span></div>";
                     charIndex++;
                     charIndex++;
@@ -35,6 +42,10 @@ export class HtmlFormatter {
                     }
                 }
             }
+            html += mathLevel.getBrackets()[1] !== undefined ? mathLevel.getBrackets()[1] : "";
+            if (mathLevel.error !== "") {
+                    html += '</a>';
+                        }
             return html;
         }
         else if (!isNaN(mathLevel)) {
