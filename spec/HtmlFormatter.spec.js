96cf89249fd56e263e569583470e61e35b727fad
Onnwen <onnwen.cassitto@icloud.com>
2022-11-12 20:26:58 +0100

    Rinominato MathExpression a MathFunction

--- spec/HtmlFormatter.spec.js
+++ spec/HtmlFormatter.spec.js
@@ -1,23 +1,23 @@
-import {MathExpression} from '../src/MathExpression.js';
+import {MathFunction} from '../src/MathFunction.js';
 
 describe('HtmlFormatter', () => {
     it('formatting: "1-2"', () => {
-        expect(new MathExpression("1-2").getHtml()).toEqual('1&minus;2');
+        expect(new MathFunction("1-2").getHtml()).toEqual('1&minus;2');
     });
 
     it('formatting: "1+2"', () => {
-        expect(new MathExpression("1+2").getHtml()).toEqual('1&plus;2');
+        expect(new MathFunction("1+2").getHtml()).toEqual('1&plus;2');
     });
 
     it('formatting: "1*2"', () => {
-        expect(new MathExpression("1*2").getHtml()).toEqual('1&times;2');
+        expect(new MathFunction("1*2").getHtml()).toEqual('1&times;2');
     });
 
     it('formatting: "1^2"', () => {
-        expect(new MathExpression("1^2").getHtml()).toEqual('1<sup>2</sup>');
+        expect(new MathFunction("1^2").getHtml()).toEqual('1<sup>2</sup>');
     });
 
     it('formatting: "1/2"', () => {
-        expect(new MathExpression("1/2").getHtml()).toEqual("<div class=\'frac\'><span>1</span><span class=\'symbol\'>/</span><span class=\'bottom\'>2</span></div>");
+        expect(new MathFunction("1/2").getHtml()).toEqual("<div class=\'frac\'><span>1</span><span class=\'symbol\'>/</span><span class=\'bottom\'>2</span></div>");
     });
 });
--- spec/MathExpression.spec.js
+++ spec/MathExpression.spec.js
@@ -1,39 +1,39 @@
-import {MathExpression} from '../src/MathExpression.js';
+import {MathFunction} from '../src/MathFunction.js';
 
 describe('MathExpression', () => {
     it('formatting: "1-2"', () => {
-        expect(new MathExpression("1-2").getJson()).toEqual('{"level":{"level":["1","-","2"],"brackets":"","error":""}}');
+        expect(new MathFunction("1-2").getJson()).toEqual('{"expression":{"level":["1","-","2"],"brackets":"","error":""}}');
     });
 
     it('formatting: "1-2+(3-4)"', () => {
-        expect(new MathExpression("1-2+(3-4)").getJson()).toEqual('{"level":{"level":["1","-","2","+",{"level":["3","-","4"],"brackets":"()","error":""}],"brackets":"","error":""}}');
+        expect(new MathFunction("1-2+(3-4)").getJson()).toEqual('{"expression":{"level":["1","-","2","+",{"level":["3","-","4"],"brackets":"()","error":""}],"brackets":"","error":""}}');
     });
 
     it('formatting: "(1-2)+3+4"', () => {
-        expect(new MathExpression("(1-2)+3+4").getJson()).toEqual('{"level":{"level":[{"level":["1","-","2"],"brackets":"()","error":""},"+","3","+","4"],"brackets":"","error":""}}');
+        expect(new MathFunction("(1-2)+3+4").getJson()).toEqual('{"expression":{"level":[{"level":["1","-","2"],"brackets":"()","error":""},"+","3","+","4"],"brackets":"","error":""}}');
     });
 
     it('formatting: "1-2+(3-4)+3"', () => {
-        expect(new MathExpression("1-2+(3-4)+3").getJson()).toEqual('{"level":{"level":["1","-","2","+",{"level":["3","-","4"],"brackets":"()","error":""},"+","3"],"brackets":"","error":""}}');
+        expect(new MathFunction("1-2+(3-4)+3").getJson()).toEqual('{"expression":{"level":["1","-","2","+",{"level":["3","-","4"],"brackets":"()","error":""},"+","3"],"brackets":"","error":""}}');
     });
 
     it('formatting: "1-2(3-4)+3"', () => {
-        expect(new MathExpression("1-2(3-4)+3").getJson()).toEqual('{"level":{"level":["1","-","2","*",{"level":["3","-","4"],"brackets":"()","error":""},"+","3"],"brackets":"","error":""}}');
+        expect(new MathFunction("1-2(3-4)+3").getJson()).toEqual('{"expression":{"level":["1","-","2","*",{"level":["3","-","4"],"brackets":"()","error":""},"+","3"],"brackets":"","error":""}}');
     });
 
     it('formatting: "1-2+{3+[(4+5)+6+(7+8)]+9}+10"', () => {
-        expect(new MathExpression("1-2+{3+[(4+5)+6+(7+8)]+9}+10").getJson()).toEqual('{"level":{"level":["1","-","2","+",{"level":["3","+",{"level":[{"level":["4","+","5"],"brackets":"()","error":""},"+","6","+",{"level":["7","+","8"],"brackets":"()","error":""}],"brackets":"[]","error":""},"+","9"],"brackets":"{}","error":""},"+","10"],"brackets":"","error":""}}');
+        expect(new MathFunction("1-2+{3+[(4+5)+6+(7+8)]+9}+10").getJson()).toEqual('{"expression":{"level":["1","-","2","+",{"level":["3","+",{"level":[{"level":["4","+","5"],"brackets":"()","error":""},"+","6","+",{"level":["7","+","8"],"brackets":"()","error":""}],"brackets":"[]","error":""},"+","9"],"brackets":"{}","error":""},"+","10"],"brackets":"","error":""}}');
     });
 
     it('formatting: "2*x"', () => {
-        expect(new MathExpression("2*x").getJson()).toEqual('{"level":{"level":["2","*","x"],"brackets":"","error":""}}');
+        expect(new MathFunction("2*x").getJson()).toEqual('{"expression":{"level":["2","*","x"],"brackets":"","error":""}}');
     });
 
     it('formatting: "2x"', () => {
-        expect(new MathExpression("2x").getJson()).toEqual('{"level":{"level":["2","*","x"],"brackets":"","error":""}}');
+        expect(new MathFunction("2x").getJson()).toEqual('{"expression":{"level":["2","*","x"],"brackets":"","error":""}}');
     });
 
     it('formatting: "3.14"', () => {
-        expect(new MathExpression("3.14").getJson()).toEqual('{"level":{"level":["3,14"],"brackets":"","error":""}}');
+        expect(new MathFunction("3.14").getJson()).toEqual('{"expression":{"level":["3,14"],"brackets":"","error":""}}');
     });
 });
