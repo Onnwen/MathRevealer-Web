0e77a82ec86b3d692caf5ae413a3f2b4fd9aacb3
Onnwen <onnwen.cassitto@icloud.com>
2022-11-12 14:24:31 +0100

    Aggiunto test case con parentesi annidate

--- spec/MathExpression.spec.js
+++ spec/MathExpression.spec.js
@@ -21,6 +21,10 @@ describe('MathExpression', () => {
         expect(new MathExpression("1-2(3-4)+3").getJson()).toEqual('{"level":{"level":["1","-","2","*",{"level":["3","-","4"],"brackets":"()","error":""},"+","3"],"brackets":"","error":""}}');
     });
 
+    it('formatting: "1-2+{3+[(4+5)+6]+7}+8"', () => {
+        expect(new MathExpression("1-2+{3+[(4+5)+6]+7}+8").getJson()).toEqual('{"level":{"level":["1","-","2","+",{"level":["3","+",{"level":[{"level":["4","+","5"],"brackets":"()","error":""},"+","6"],"brackets":"[]","error":""},"+","7"],"brackets":"{}","error":""},"+","8"],"brackets":"","error":""}}');
+    });
+
     it('formatting: "2*x"', () => {
         expect(new MathExpression("2*x").getJson()).toEqual('{"level":{"level":["2","*","x"],"brackets":"","error":""}}');
     });
