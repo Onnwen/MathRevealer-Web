1f2ab2e3bf04d6ad6d0fae62d859c22b7c2a670f
Onnwen <onnwen.cassitto@icloud.com>
2022-11-12 16:25:38 +0100

    Aggiunto supporto alle radici quadrate

--- src/Symbol.js
+++ src/Symbol.js
@@ -1,6 +1,6 @@
 export class Symbol {
-    static operationSymbols = ["+", "-", "*", "/", "^"];
-    static priorityOperationSymbols = ["*", "/", "^"];
+    static operationSymbols = ["+", "-", "*", "/", "^", "#"];
+    static priorityOperationSymbols = ["*", "/", "^", "#"];
     static openingBracketSymbols = ["(", "{", "["];
     static closingBracketSymbols = [")", "}", "]"];
     static variableSymbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
