5534accef633846e5e616cd498281836be768de4
Onnwen <onnwen.cassitto@icloud.com>
2022-11-12 20:21:32 +0100

    Migliorata efficienza ignorando spazi

--- src/MathExpression.js
+++ src/MathExpression.js
@@ -12,23 +12,25 @@ export class MathExpression {
         let workingLevels = [this.level];
 
         for (let charIndex = 0; charIndex < expression.length; charIndex++) {
-            if (Symbol.isClosingBracket(expression[charIndex])) {
-                workingLevels.at(-1).closeBrackets(expression[charIndex]);
-                workingLevels.pop();
-            }
-            else if (Symbol.isOpeningBracket(expression[charIndex])) {
-                if (Symbol.isValue(workingLevels.at(-1).getLastChar())) {
-                    workingLevels.at(-1).getLevel().push("*");
+            if (expression[charIndex] !== "") {
+                if (Symbol.isClosingBracket(expression[charIndex])) {
+                    workingLevels.at(-1).closeBrackets(expression[charIndex]);
+                    workingLevels.pop();
                 }
-                workingLevels.push(new MathLevel());
-                workingLevels.at(-1).brackets = expression[charIndex];
-                workingLevels.at(-2).getLevel().push(workingLevels.at(-1));
-            }
-            else if (charIndex > 0 && !Symbol.isValid(expression[charIndex-1])) {
-                workingLevels.at(workingLevels.at(-2) !== undefined ? -2 : -1).addChar(expression[charIndex])
-            }
-            else {
-                workingLevels.at(-1).addChar(expression[charIndex]);
+                else if (Symbol.isOpeningBracket(expression[charIndex])) {
+                    if (Symbol.isValue(workingLevels.at(-1).getLastChar())) {
+                        workingLevels.at(-1).getLevel().push("*");
+                    }
+                    workingLevels.push(new MathLevel());
+                    workingLevels.at(-1).brackets = expression[charIndex];
+                    workingLevels.at(-2).getLevel().push(workingLevels.at(-1));
+                }
+                else if (charIndex > 0 && !Symbol.isValid(expression[charIndex-1])) {
+                    workingLevels.at(workingLevels.at(-2) !== undefined ? -2 : -1).addChar(expression[charIndex])
+                }
+                else {
+                    workingLevels.at(-1).addChar(expression[charIndex]);
+                }
             }
         }
     }
