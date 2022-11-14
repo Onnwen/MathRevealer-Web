081e39e46d26fad90596aae7b7d6827b1d2c0fdc
Onnwen <onnwen.cassitto@icloud.com>
2022-11-14 17:08:25 +0100

    Aggiunti metodi getSet(), canBeCombinetWith(), combineWith e risolto problema di esportazione

--- src/math/domain/MathExistenceCondition.js
+++ src/math/domain/MathExistenceCondition.js
@@ -1,7 +1,46 @@
-class MathExistenceCondition {
+export class MathExistenceCondition {
     constructor(value, sign, set) {
         this.value = value;
         this.sign = sign;
         this.set = set;
+    }
+
+    getSet() {
+        if (typeof this.set === 'object') {
+            return this.set.getSet();
+        }
+        else {
+            return this.set;
+        }
+    }
+
+    canBeCombinedWith(existenceCondition) {
+        return this.value === existenceCondition.value && this.sign.charAt(0) === existenceCondition.sign.charAt(0) && (this.sign.charAt(0) === "<" || this.sign.charAt(0) === ">");
+    }
+
+    combineWith(existenceCondition) {
+        if (this.canBeCombinedWith(existenceCondition)) {
+            switch (this.sign.at(0)) {
+                case ">":
+                    if ((this.sign === ">=" && this.getSet() > existenceCondition.getSet()) || (existenceCondition.sign === ">=" && existenceCondition.getSet() > this.getSet()) || (this.sign === ">=" && existenceCondition.sign === ">=")) {
+                        return new MathExistenceCondition(this.value, ">=", this.getSet() > existenceCondition.getSet() ? this.set : existenceCondition.set);
+                    } else {
+                        return new MathExistenceCondition(this.value, ">", this.getSet() > existenceCondition.getSet() ? this.set : existenceCondition.set);
+                    }
+                case "<":
+                    if ((this.sign === "<=" && this.getSet() < existenceCondition.getSet()) || (existenceCondition.sign === ">=" && existenceCondition.getSet() < this.getSet()) || (this.sign === ">=" && existenceCondition.sign === ">=")) {
+                        return new MathExistenceCondition(this.value, "<=", this.getSet() < existenceCondition.getSet() ? this.set : existenceCondition.set);
+                    } else {
+                        return new MathExistenceCondition(this.value, "<", this.getSet() < existenceCondition.getSet() ? this.set : existenceCondition.set);
+                    }
+                default:
+                    return [this, existenceCondition];
+            }
+        }
+        return [this, existenceCondition];
+    }
+
+    getJson() {
+        return JSON.stringify(this);
     }
 }
