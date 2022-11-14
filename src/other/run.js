2f73b82ce1f6add53f6a0a8e3e130aca71d76aa6
Onnwen <onnwen.cassitto@icloud.com>
2022-11-14 20:22:44 +0100

    Update run.js

--- src/other/run.js
+++ src/other/run.js
@@ -1,3 +1,10 @@
+import {MathDomain} from "../math/domain/MathDomain.js";
+import {MathExistenceCondition} from "../math/domain/MathExistenceCondition.js";
 import {MathFunction} from "../math/function/MathFunction.js";
 
-console.log(new MathFunction("1-2+(3-4)"))
+// console.log(new MathExistenceCondition("x", ">", "6").getJson());
+
+let a = "-2";
+let b = "-4";
+
+console.log(new MathFunction("[3/(x-2/2)]+x").getLaTeX());
