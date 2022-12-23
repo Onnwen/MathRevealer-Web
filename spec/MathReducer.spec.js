cf965a25973f1fca13cf7af9f14dcd8bcb510e88
Onnwen <onnwen.cassitto@icloud.com>
2022-12-23 13:55:17 +0100

    Cambio nome metodo

--- spec/MathReducer.spec.js
+++ spec/MathReducer.spec.js
@@ -3,6 +3,6 @@ import {MathFunction} from "../src/Algorithms/Function/MathFunction.js";
 
 describe('MathReduce', () => {
     it('reducing: "1-2"', () => {
-        expect(MathReducer.getSolvedMathLevel(new MathFunction("2-1").getExpression()).toString()).toEqual('{"expression":{"level":["1","-","2"],"brackets":"","error":""}}');
+        expect(MathReducer.analyse(new MathFunction("2-1").getExpression()).toString()).toEqual('{"expression":{"level":["1","-","2"],"brackets":"","error":""}}');
     });
 });
--- src/other/run.js
+++ src/other/run.js
@@ -1,4 +1,4 @@
 import {MathFunction} from "../Algorithms/Function/MathFunction.js";
 import {MathReducer} from "../Algorithms/Function/MathReducer.js";
 
-console.log(MathReducer.getSolvedMathLevel(new MathFunction("2+1+5-10").getExpression()))
+console.log(MathReducer.analyse(new MathFunction("2+1+5/2-10").getExpression()))
