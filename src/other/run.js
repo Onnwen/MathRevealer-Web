a3f52a75e04bd689cf7e2009492ed239e182908e
Onnwen <onnwen.cassitto@icloud.com>
2022-12-24 12:57:25 +0100

    File dovrebbe essere ignorato da .gitignore

--- src/other/run.js
+++ src/other/run.js
@@ -1,4 +1,11 @@
 import {MathFunction} from "../Algorithms/Function/MathFunction.js";
 import {MathReducer} from "../Algorithms/Function/MathReducer.js";
 
-console.log(MathReducer.analyse(new MathFunction("2+1+5/2-10").getExpression()))
+const expression = new MathFunction("8*3+5*x+2-7*9-12*x-8+1-2*3").getExpression();
+// const expression = new MathFunction("1-2").getExpression();
+expression.printDebug();
+
+console.log("--------------------");
+
+const analised = MathReducer.analyse(expression);
+analised.printDebug();
