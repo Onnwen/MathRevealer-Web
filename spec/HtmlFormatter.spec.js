cbb418a199b045669f002b80e0c9ec52b1980cd4
Onnwen <onnwen.cassitto@icloud.com>
2022-12-26 16:29:19 +0100

    Aggiornati test

--- spec/HtmlFormatter.spec.js
+++ spec/HtmlFormatter.spec.js
@@ -1,19 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const MathFunction_1 = require("../src/Algorithms/Function/MathFunction");
+const MathFunction_1 = require('../dist/Algorithms/Function/MathFunction');
+
 describe('HtmlFormatter', () => {
     it('formatting: "1-2"', () => {
         expect(new MathFunction_1.MathFunction("1-2").getHtml()).toEqual('1&minus;2');
     });
+
     it('formatting: "1+2"', () => {
         expect(new MathFunction_1.MathFunction("1+2").getHtml()).toEqual('1&plus;2');
     });
+
     it('formatting: "1*2"', () => {
         expect(new MathFunction_1.MathFunction("1*2").getHtml()).toEqual('1&times;2');
     });
+
     it('formatting: "1^2"', () => {
         expect(new MathFunction_1.MathFunction("1^2").getHtml()).toEqual('1<sup>2</sup>');
     });
+
     it('formatting: "1/2"', () => {
         expect(new MathFunction_1.MathFunction("1/2").getHtml()).toEqual("<div class=\'frac\'><span>1</span><span class=\'symbol\'>/</span><span class=\'bottom\'>2</span></div>");
     });
--- spec/MathDomain.spec.js
+++ spec/MathDomain.spec.js
@@ -1,13 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const MathDomain_ts_1 = require("../src/Algorithms/Domain/MathDomain.ts");
-const MathExistenceCondition_ts_1 = require("../src/Algorithms/Domain/MathExistenceCondition.ts");
+const MathDomain_1 = require('../dist/Algorithms/Domain/MathDomain');
+const MathExistenceCondition = require('../dist/Algorithms/Domain/MathExistenceCondition');
+
 describe('MathDomain', () => {
     it('calculating domain with existence conditions: x>3, x>6', () => {
-        let mathDomain = new MathDomain_ts_1.MathDomain();
-        mathDomain.addExistenceCondition(new MathExistenceCondition_ts_1.MathExistenceCondition("x", ">", "3"));
-        mathDomain.addExistenceCondition(new MathExistenceCondition_ts_1.MathExistenceCondition("x", ">", "6"));
+        let mathDomain = new MathDomain_1.MathDomain();
+        mathDomain.addExistenceCondition(new MathExistenceCondition.MathExistenceCondition("x", ">", "3"));
+        mathDomain.addExistenceCondition(new MathExistenceCondition.MathExistenceCondition("x", ">", "6"));
         mathDomain.calculateDomain();
-        expect(mathDomain.getJson()).toEqual('[{"value":"x","sign":">","set":"6"}]');
+        expect(mathDomain.getJson()).toEqual('[{"_value":"x","_sign":">","_set":"6"}]');
     });
 });
