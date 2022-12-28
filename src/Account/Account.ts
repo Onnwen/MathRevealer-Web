f7fd1f30619b7f29aa2a744e5a7a42bf6ffbea51
Onnwen <onnwen.cassitto@icloud.com>
2022-12-28 21:29:59 +0100

    Sviluppate classi UserInformation e Account

---
+++ src/Account/Account.ts
@@ -0,0 +1,77 @@
+class UserInformation {
+    email: string;
+    first_name: string;
+    last_name: string;
+    email_verified: boolean;
+
+    constructor(email: string, first_name: string, last_name: string, email_verified: boolean) {
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email_verified = email_verified;
+    }
+}
+
+class Account {
+    static async loadAccountData() {
+        return $.ajax({
+            url: 'https://mathrevealer.garamante.it/api/users/myaccount',
+            type: 'GET',
+            success: function (data) {
+                if (data.status_code == 1) {
+                    this.setUserInformation(data.userInformation);
+                    return data;
+                } else {
+                    localStorage.clear();
+                    return undefined
+                }
+            },
+            error: function (data) {
+                localStorage.clear();
+                return undefined
+            }
+        });
+    }
+
+    static setUserInformation(userInformation: any) {
+        localStorage.setItem('id', userInformation.id);
+        localStorage.setItem('email', userInformation.email);
+        localStorage.setItem('first_name', userInformation.first_name);
+        localStorage.setItem('last_name', userInformation.last_name);
+        localStorage.setItem('email_verified', String(userInformation.email_verified));
+    }
+
+    static async getEmail() {
+        if (localStorage.getItem('email') == undefined) {
+            if (await this.loadAccountData()) {
+                return localStorage.getItem('email');
+            }
+            return undefined;
+        } else {
+            return localStorage.getItem('email');
+        }
+    }
+
+    static async getId() {
+        if (localStorage.getItem('id') == undefined) {
+            if (await this.loadAccountData()) {
+                return localStorage.getItem('id');
+            }
+            return undefined;
+        } else {
+            return localStorage.getItem('id');
+        }
+    }
+
+    static resendVerificationEmail() {
+
+    }
+
+    static async isLogged() {
+        return await this.getEmail() != undefined && localStorage.getItem('email_verified');
+    }
+
+    static clear() {
+        localStorage.clear();
+    }
+}
