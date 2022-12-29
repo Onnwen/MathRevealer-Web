9cea13ff5e16bedf08d2a0c9fbe4a728e031f82c
Onnwen <onnwen.cassitto@icloud.com>
2022-12-29 18:53:32 +0100

    Rimosso file di debug non inerente al progetto in sé

--- src/Other/run.ts
+++
@@ -1,6 +0,0 @@
-import {MathFunction} from "../Algorithms/Function/MathFunction";
-import {MathReducer} from "../Algorithms/Calculator/MathReducer";
-import {MathSolver} from "../Algorithms/Calculator/MathSolver";
-
-const expression = new MathFunction("8+2x+4-4+2").expression;
-console.log(expression.getX());
