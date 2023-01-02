ffb4c34fe87ca2cebf0336c789c0809a9ecd35b9
Onnwen <onnwen.cassitto@icloud.com>
2023-01-02 15:10:46 +0100

    Implementati widget allargabili e nozioni teoriche

--- src/UI/UIMathCard.ts
+++ src/UI/UIMathCard.ts
@@ -39,36 +39,47 @@ export class UIMathCard {
         this._notion = value;
     }
 
-    constructor(title: string, description: string, notion: string = "", html: string = "") {
+    private _expandable: boolean;
+
+    set expandable(value: boolean) {
+        this._expandable = value;
+    }
+
+    get expandable(): boolean {
+        return this._expandable;
+    }
+
+    constructor(title: string, description: string, notion: string = "", html: string = "", expandable: boolean = true) {
         this._title = title;
         this._description = description;
         this._html = html;
         this._notion = notion;
+        this._expandable = expandable;
     }
 
     getHtml(): string {
-        return `<div class="mathResultCard" id="${this._title}Card">` +
-            `<button class="vertical mathResultCardEnlargeButton" id="${this._title}CardButton" type="button" style="float: right; margin-bottom: auto;">` +
-            // `<i id="${this._title}CardButtonSizeIcon" class="bi bi-arrows-angle-expand d-none d-md-flex"></i>` +
-            `</button>` +
+        return `<div class="mathResultCard ${this.expandable ? "expandable" : "" }" id="${this._title}Card">` +
+            `<i class="bi bi-arrows-angle-expand d-md-flex" style="float: right; margin-bottom: auto;"></i>` +
             `<h2> ${this._title} </h2>` +
             `<hr/>` +
             (this._description !== undefined && this._description !== "" ?
                 `<p class="mathTextResultDescription">${this._description}</p>`
-            :
+                :
                 `<p class="mathTextResultDescription">Non è stato possibile trovare nessun dato in merito.</>`) +
             (this._html !== undefined && this._html !== "" ?
                 `<hr/>` +
                 `<div class="mathTextResult">${this._html}</div>`
-            :
+                :
                 "")
             +
-            // (this._notion !== undefined && this._notion !== "" ?
-            //     `<hr/>` +
-            //     `<p class="mathTextResultDescription">${this._notion}</p>`
-            //     :
-            //     "")
-            // +
+            (this._notion !== undefined && this._notion !== "" ?
+                `<div class="mathTexResultTheory"/>` +
+                `<hr/>` +
+                `<p class="mathTextTheory">${this._notion}</p>` +
+                `</div>`
+                :
+                "")
+            +
             `</div>`;
     }
 }
